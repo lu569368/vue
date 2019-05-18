10d0e64727404d741467142650e4aca10ba78089
lu569368 <569368665@qq.com>
2019-05-18 22:14:21 +0800

    20190518

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="ce">
+      <home-header></home-header>
+   	  hello word
+   </div>
+</template>
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components:{
+  	HomeHeader:HomeHeader
+  }
+}
+</script>
+<style>
+	
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="header">
+   	  <div class="header-left"></div>
+   	  <div class="header-input"></div>
+   	  <div class="header-right"></div>
+   </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader',
+}
+</script>
+<style   scoped>
+	.header
+</style>
