c8c0d5c0753d5ae4dd2550ad8e5895ae9b924473
安青非 <569368665@qq.com>
2019-05-20 17:41:41 +0800

    20190520

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,35 +1,35 @@
 <template>
-   <div class="header">
-   	  <div class="header-left">111</div>
-   	  <div class="header-input">沉甸甸</div>
-   	  <div class="header-right">333</div>
-   </div>
+  <div class="header">
+    <div class='header-left'>返回</div>
+    <div class='header-input'>输入</div>
+    <div class='header-right'>城市</div>
+  </div>
 </template>
 <script>
 export default {
   name: 'HomeHeader',
 }
 </script>
-<style lang="stylus"  scoped>
-	.header
-	    display:flex
-	    line-height:.86rem
-	    background:#00bcd4
-	    color:#fff
-	    .header-left
-	       width:.64rem
-	       float:left
-	       text-align:center
-	    .header-input
-	       flex:1
-	       height:.64rem
-	       line-height:.64rem
-	       margin-left:.2rem
-	       margin-top:.12rem
-	       background:#fff
-	       border-radius:.1rem
-	       color:#000
-	    .header-right
-	       width:1.24rem
-	       float:right
+<style lang='stylus'  scoped>
+  .header
+    display:flex
+    line-height:.86rem
+    background:#00bcd4
+    color:#fff
+    .header-left
+      width:.64rem
+      float:left
+      text-align:center
+    .header-input
+      flex:1
+      height:.64rem
+      line-height:.64rem
+      margin-left:.2rem
+      margin-top:.12rem
+      background:#fff
+      border-radius:.1rem
+      color:#000
+    .header-right
+      width:1.24rem
+      float:right
 </style>
